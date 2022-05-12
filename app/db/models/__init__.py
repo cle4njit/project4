b4916cf34883e96ab9f23f315311964fe45bb061
charles <cle4@njit.edu>
2022-05-12 03:44:49 -0400

    database init modified

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,13 +1,34 @@
 from datetime import datetime
-
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.db import db
 from flask_login import UserMixin
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy_serializer import SerializerMixin
+
+Base = declarative_base()
+
+
+class Transaction(db.Model, SerializerMixin):
+    __tablename__ = 'transactions'
+    id = db.Column(db.Integer, primary_key=True)
+    AMOUNT = db.Column(db.Float(), nullable=False, unique=False)
+    TYPE = db.Column(db.String(6), nullable=False, unique=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = relationship("User", back_populates="transactions", uselist=False)
+
+    def __init__(self, AMOUNT, TYPE):
+        self.AMOUNT = AMOUNT
+        self.TYPE = TYPE
+
+    def serialize(self):
+        return {
+            'AMOUNT': self.AMOUNT,
+            'TYPE': self.TYPE,
+        }
 
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
-
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(300), nullable=False, unique=True)
@@ -16,14 +37,17 @@ class User(UserMixin, db.Model):
     registered_on = db.Column('registered_on', db.DateTime)
     active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
     is_admin = db.Column('is_admin', db.Boolean(), nullable=False, server_default='0')
+    balance = db.Column(db.Float, nullable=True, unique=False, default="0.00")
+    transactions = db.relationship("Transaction", back_populates="user", cascade="all, delete")
 
     # `roles` and `groups` are reserved words that *must* be defined
     # on the `User` model to use group- or role-based authorization.
 
-    def __init__(self, email, password):
+    def __init__(self, email, password, is_admin):
         self.email = email
         self.password = password
         self.registered_on = datetime.utcnow()
+        self.is_admin = is_admin
 
     def is_authenticated(self):
         return True
